cb7baed4655bb051b1ee87598922fc6a930f7885
969415431@qq.com <969415431@qq.com>
2020-02-13 14:54:38 +0800

    update apis.js

--- routes/apis.js
+++ routes/apis.js
@@ -3,58 +3,58 @@ const ipfs = ipfsClient('http://localhost:5001');
 var express = require('express');
 var router = express.Router();
 
-router.post('/survey/:id', function(req, res) {
+router.post('/survey/:id', async function(req, res) {
     if (JSON.stringify(req.body) === '{}') return res.sendStatus(400);
     survey_id = req.params.id;
     dir_path = '/' + survey_id;
     path =  dir_path + '/survey_' + survey_id + '.json';
     //处理相同survey id已存在的情况
-    // return res.sendStatus(400);
-    //处理全新survey
-    async function new_survey() {
-        try {
-            await ipfs.files.mkdir(survey_id);
-            await ipfs.files.write(path, Buffer.from(JSON.stringify(req.body)), { create: true })
-        } catch(e) {
-            return res.status(500).send('faild to due with survey ' + survey_id);
-        }
-        var { cid } = await ipfs.files.stat(dir_path)
-        // var reg = /CID\(\'(.*)\'\)/;
-        // hash = reg.exec(hash)[1].trim();
-        return cid.toString();
+    //存在则删除映射关系，实际文件不会被删除，可通过hash值查找
+    try {
+        var stat = await ipfs.files.stat(dir_path);
+        await ipfs.files.rm(dir_path, { recursive: true });
+    } catch (e) {
+        console.log(e);
     }
-    var hash = new_survey();
-    res.json({ survey_hash: hash});
+    //处理全新survey
+    try {
+        await ipfs.files.mkdir(dir_path);
+        await ipfs.files.write(path, Buffer.from(JSON.stringify(req.body)), { create: true })
+    } catch(e) {
+        conlose.log(e)
+        return res.status(500).send('faild to due with survey ' + survey_id);
+    }
+    var { cid } = await ipfs.files.stat(dir_path)
+    await res.json({ survey_hash: cid.toString()});
 });
 
-router.post('/answer/:id', function(req, res) {
+router.post('/answer/:id', async function(req, res) {
     if (JSON.stringify(req.body) === '{}') return res.sendStatus(400);
     answer_id = req.params.id;
     survey_id = req.body.survey_id;
     dir_path = '/' + survey_id;
     path = dir_path + '/answer' + answer_id + '.json';
-    //处理相同survey id已存在的情况
-    // return res.sendStatus(400);
-
-    //处理全新answer
-    async function new_answer() {
-        try {
-            await ipfs.files.write(path, Buffer.from(JSON.stringify(req.body.answer)), { create: true });
-        } catch(e) {
-            return res.status(500).send('faild to due with survey ' + survey_id);
-        }
-
-        var { cid } = await ipfs.files.stat(dir_path);
-        var survey_hash = cid.toString();
-        //var reg = /CID\(\'(.*)\'\)/;
-        //survey_hash = reg.exec(hash)[1].trim();
-
-        var { cid } = await ipfs.files.stat(path);
-        var answer_hash = cid.toString();
-        return { "survey": survey_hash, "answer": answer_hash};
+    //处理相同answer id已存在的情况
+    //存在则删除映射关系，实际文件不会被删除，可通过hash值查找
+    try {
+        var stat = await ipfs.files.stat(path);
+        await ipfs.files.rm(path);
+    } catch (e) {
+        console.log(e);
     }
-    var ans = new_answer();
-    res.json(ans);
+    //处理全新answer
+    try {
+        await ipfs.files.write(path, Buffer.from(JSON.stringify(req.body.answer)), { create: true });
+    } catch(e) {
+        return res.status(500).send('faild to due with survey ' + survey_id);
+    }
+
+    var { cid } = await ipfs.files.stat(dir_path);
+    var survey_hash = cid.toString();
+
+    var { cid } = await ipfs.files.stat(path);
+    var answer_hash = cid.toString();
+    await res.json({ "survey": survey_hash, "answer": answer_hash});
 });
 
 module.exports = router;
